ca7bb7c02e1a90b234ed86f4894178ab9e4d3641
Bo Jansson <bobjjansson@gmail.com>
2017-06-03 18:26:31 +0200

    act part added

---
+++ TDD/TravelAgencyTests/TourSchedule.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TravelAgencyTests
+{
+    internal class TourSchedule
+    {
+        public TourSchedule()
+        {
+        }
+
+        internal void CreateTour(string v1, DateTime dateTime, int v2)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TDD/TravelAgencyTests/TourScheduleTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelAgencyTests
+{
+    [TestFixture]
+    public class TourScheduleTests
+    {
+        [Test]
+        public void CanCreateNewTour()
+        {
+            //arr part
+            var sut = new TourSchedule();
+            sut.CreateTour(
+            "New years day safari",
+            new DateTime(2013, 1, 1), 20);
+
+
+        }
+    }
+}
